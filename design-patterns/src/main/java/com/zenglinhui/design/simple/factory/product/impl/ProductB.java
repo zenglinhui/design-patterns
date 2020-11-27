1d33bba4b89b00babf933eda0d49040507d90571
曾林辉 <linhui.ceng@sfmail.sf-express.com>
2020-11-27 19:02:47 +0800

    add commit

--- design-patterns/src/main/java/com/zenglinhui/design/simple/factory/product/impl/ProductB.java
+++ design-patterns/src/main/java/com/zenglinhui/design/simple/factory/product/impl/ProductB.java
@@ -7,7 +7,8 @@ import com.zenglinhui.design.simple.factory.product.Product;
  * @date 2020/11/27 18:47
  */
 public class ProductB implements Product {
-    @Override
+
+
     public String desc() {
         return "产品B";
     }
